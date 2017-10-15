9e02ebbaac3abee0c668da8c5965783ad80efca1
Ryo Kato <katoryo55@gmail.com>
2017-10-15 14:12:25 -0700

    Implement a little

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import changeMood from './change-mood';
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.getElementById('js-btn-change-mood');
+    btn.onclick = changeMood;
+
+    return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
