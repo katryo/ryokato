43be1b2da7931264016997b813970037c05bc3a1
Ryo Kato <katoryo55@gmail.com>
2017-10-21 16:58:14 -0700

    Use dist dir

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import changeMood from './change-mood';
+import Ocean from './ocean.jpg';
 
 function component() {
     const element = document.createElement('div');
-    const btn = document.getElementById('js-btn-change-mood');
-    btn.onclick = changeMood;
 
+    const btn = document.getElementById('js-btn-change-mood');
+
+    const bg = document.getElementById('js-bg');
+    const main = document.getElementById('js-main');
+
+    const setNormalMode = function() {
+        bg.style.opacity = '1';
+        main.style.color = 'black';
+        main.style.backgroundColor = 'rgba(255, 255, 255, 0.3)';
+        btn.onclick = setOceanMode;
+    }
+
+    const doAndWait = function(fun) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(fun());
+            }, 200);
+        })
+    }
+
+    const decreaseOpacity = function(fun) {
+        doAndWait(function () { bg.style.opacity = '0.9'})
+            .then(doAndWait(function () {
+                bg.style.opacity = '0.8';
+            }))
+            .then(doAndWait(function () {
+                bg.style.opacity = '0.7';
+            }))
+            .then(doAndWait(function () {
+                bg.style.opacity = '0.4';
+            }))
+            .then(doAndWait(function () {
+                bg.style.opacity = '0.2';
+            }))
+            .then(fun());
+    }
+
+    const setOceanMode = function() {
+
+        const time = window.performance.now();
+
+        window.requestAnimationFrame(function fade)
+
+            decreaseOpacity(function () {
+                main.style.color = 'white';
+                main.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+                btn.onclick = setNormalMode;
+            }
+        );
+    }
+
+    btn.onclick = setOceanMode;
+
+    document.body.setAttribute('background', Ocean);
+    document.body.style.backgroundSize = "cover";
     return element;
 }
 
--- upload-to-s3.sh
+++ upload-to-s3.sh
@@ -1,1 +1,1 @@
-aws s3 cp public s3://ryokato.com/ --recursive --exclude ".DS_Store"
+aws s3 cp dist s3://ryokato.com/ --recursive --exclude ".DS_Store"
